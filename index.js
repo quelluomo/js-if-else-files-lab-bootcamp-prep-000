e470652a69e232b9d325512fdc1775121a4108be
Matteo Muscas <thatman.j@gmail.com>
2018-02-20 11:00:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var name = "Joe"
 var height = "74"
 if (name=== "Susan") {
   console.log("The name is correct")
-}
-else var name = false {
+} else {
   console.log("Expected the name to be Susan")
 }
 
